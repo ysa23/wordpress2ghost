3fc329edc939866557de182f73b2fe142b47129f
yossi <yossi@delver.com>
2014-06-18 20:11:03 +0300

    Removing redundant fields from ghost tag

--- src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter.Domain/Ghost/GhostTag.cs
+++ src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter.Domain/Ghost/GhostTag.cs
@@ -6,13 +6,7 @@ namespace YsA.Wordpress2GhostImporter.Domain.Ghost
 	{
 		public long Id { get; set; }
 		public string Name { get; set; }
-		public string Description { get; set; }
-		public long? ParentId { get; set; }
-		public string MetaTitle { get; set; }
-		public string MetaDescription { get; set; }
 		public DateTime CreatedAt { get; set; }
 		public long CreatedBy { get; set; }
-		public DateTime UpdatedAt { get; set; }
-		public long UpdatedBy { get; set; }
 	}
 }
