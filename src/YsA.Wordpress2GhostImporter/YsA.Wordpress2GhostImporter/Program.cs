3477b77ae931859aaa14a0d5c1ce6b11247faec3
yossi <yossi@delver.com>
2014-06-20 10:58:50 +0300

    Added arguments support for the importer (target url and target file)

--- src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter/Program.cs
+++ src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter/Program.cs
@@ -1,25 +1,45 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
-using Newtonsoft.Json;
 using YsA.Wordpress2GhostImporter.DataAccess.Json;
 using YsA.Wordpress2GhostImporter.DataAccess.Wordpress;
+using YsA.Wordpress2GhostImporter.Domain.Ghost;
 
 namespace YsA.Wordpress2GhostImporter
 {
 	class Program
 	{
-		static void Main()
+		static void Main(string[] args)
 		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Please specify that target site (as url) and the target file path (optional - otherwise will be written to console)");
+				return;
+			}
+
 			var container = new WindsorContainer().Install(FromAssembly.This());
 
 			var crawler = container.Resolve<IWordpressCrawler>();
 			var serializer = container.Resolve<IJsonSerializer>();
+			var converter = container.Resolve<IGhostConverter>();
 
-			var posts = crawler.Crawl("http://blogs.microsoft.co.il/ysa").First();
-			Console.WriteLine(serializer.Serialize(posts));
-			Console.WriteLine(JsonConvert.SerializeObject(posts, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+			Console.WriteLine("Starting to crawl '{0}'", args[0]);
+			var posts = crawler.Crawl(args[0]).ToArray();
+			Console.WriteLine("Finished crawling '{0}'. Converting to ghost", args[0]);
+			var ghostPosts = converter.FromPosts(posts);
+			var serialized = serializer.Serialize(ghostPosts);
+
+			if (args.Length == 1)
+			{
+				Console.WriteLine(serialized);
+				return;
+			}
+
+			Console.WriteLine("Finished serializing. Writing to '{0}'", args[1]);
+			File.WriteAllText(args[1], serialized);
+			Console.WriteLine("Done. All you have to do now is to import to ghost... Enjoy :)");
 		}
 	}
 }
