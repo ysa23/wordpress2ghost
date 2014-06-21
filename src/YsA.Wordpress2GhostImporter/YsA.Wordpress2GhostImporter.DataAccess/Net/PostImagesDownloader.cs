b9c45285f73be8b6d6d5aca0b506a8b142866ff3
yossi <yossi@delver.com>
2014-06-21 22:09:08 +0300

    Added downloader for all the posts images

---
+++ src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter.DataAccess/Net/PostImagesDownloader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using YsA.Wordpress2GhostImporter.Domain.Blog;
+using YsA.Wordpress2GhostImporter.Domain.Net;
+
+namespace YsA.Wordpress2GhostImporter.DataAccess.Net
+{
+	public class PostImagesDownloader : IPostImagesDownloader
+	{
+		private readonly IImageDownloader _imageDownloader;
+
+		public PostImagesDownloader(IImageDownloader imageDownloader)
+		{
+			_imageDownloader = imageDownloader;
+		}
+
+		public void DownloadImages(IList<Post> posts, string targetFolder)
+		{
+			Directory.CreateDirectory(targetFolder);
+
+			foreach(var post in posts.Where(x => x.Images != null && x.Images.Count > 0))
+			{
+				var postDirectoryPath = Path.Combine(targetFolder, RemoveInvalidCharacters(post.Title));
+				Directory.CreateDirectory(postDirectoryPath);
+
+				foreach (var image in post.Images)
+				{
+					_imageDownloader.DownloadImage(image.Url, Path.Combine(postDirectoryPath, image.Name));
+				}
+			}
+		}
+
+		private static string RemoveInvalidCharacters(string name)
+		{
+			var invalidPathCharacters = string.Format(@"([{0}]*\.+$)|([{0}]+)", Regex.Escape(new string(Path.GetInvalidFileNameChars())));
+
+			return Regex.Replace(name, invalidPathCharacters, "_");
+		}
+	}
+}
---
+++ src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter.Domain/Net/IPostImagesDownloader.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using YsA.Wordpress2GhostImporter.Domain.Blog;
+
+namespace YsA.Wordpress2GhostImporter.Domain.Net
+{
+	public interface IPostImagesDownloader
+	{
+		void DownloadImages(IList<Post> posts, string targetFolder);
+	}
+}
