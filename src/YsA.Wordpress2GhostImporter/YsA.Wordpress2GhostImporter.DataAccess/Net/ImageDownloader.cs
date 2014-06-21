56657b499d76a308e192e82366381d31514c11f1
yossi <yossi@delver.com>
2014-06-21 21:31:30 +0300

    Added image downloader

---
+++ src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter.DataAccess/Net/ImageDownloader.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using YsA.Wordpress2GhostImporter.Domain.Net;
+
+namespace YsA.Wordpress2GhostImporter.DataAccess.Net
+{
+	public class ImageDownloader : IImageDownloader
+	{
+		public void DownloadImage(string url, string target)
+		{
+			using (var webClient = new WebClient())
+			{
+				webClient.DownloadFile(url, target);
+			}
+		}
+	}
+}
---
+++ src/YsA.Wordpress2GhostImporter/YsA.Wordpress2GhostImporter.Domain/Net/IImageDownloader.cs
@@ -0,0 +1,7 @@
+﻿namespace YsA.Wordpress2GhostImporter.Domain.Net
+{
+	public interface IImageDownloader
+	{
+		void DownloadImage(string url, string target);
+	}
+}
